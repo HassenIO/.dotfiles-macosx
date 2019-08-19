e1343da426a1409e3db9592199f3334068ba2e83
Hassen Taidirt <htaidirt@gmail.com>
2019-08-19 18:15:02 +0200

    Add Hyper.js dot file

--- setup.sh
+++ setup.sh
@@ -7,11 +7,13 @@ cd tmp/
 #
 ln -s "$dotfolder/src/.zshrc" .zshrc
 ln -s "$dotfolder/src/.vimrc" .vimrc
+ln -s "$dotfolder/src/.hyper.js" .hyper.js
 
 # Move generated symbolic links to final destinations
 #
 mv .zshrc ~/.zshrc
 mv .vimrc ~/.vimrc
+mv .hyper.js ~/.hyper.js
 
 cd $dotfile
 
